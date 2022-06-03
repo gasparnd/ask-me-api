94e2c62acabced35179323288c9e9a634c399bf6
gasparnd <gaspardolcemascolo@gmail.com>
2022-06-03 17:56:49 -0300

    env variables validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'Joi';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestionsModule } from './questions/questions.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
-import { environments } from './environments';
 import { AuthModule } from './auth/auth.module';
 import config from './config';
 
@@ -18,6 +19,16 @@ import config from './config';
       envFilePath: ['.dev.env', '.prod.env'],
       isGlobal: true,
       load: [config],
+      validationSchema: Joi.object({
+        API_KEY: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        MONGO_DB: Joi.string().required(),
+        MONGO_INITDB_ROOT_USERNAME: Joi.string().required(),
+        MONGO_INITDB_ROOT_PASSWORD: Joi.string().required(),
+        MONGO_PORT: Joi.number().required(),
+        MONGO_HOST: Joi.string().required(),
+        MONGO_CONNECTION: Joi.string().required(),
+      }),
     }),
     AuthModule,
   ],
